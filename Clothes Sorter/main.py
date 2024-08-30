51d470154d1dff051187c69561c53f2cf0817fc5
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-30 23:41:01 +0800

    更新 Clothes Sorter- Closed

--- Clothes Sorter/main.py
+++ Clothes Sorter/main.py
@@ -17,3 +17,238 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+print(type(training_data))
+
+# 索引数据并可视化数据集（由matplotlib库实现）
+## 建立数据索引表，对应标签数字和实际含义
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+## 构建画布
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    print(training_data[sample_idx])
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Display image and label.
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+label_name = list(labels_map.values())[label]
+print(f"Label: {label_name}")
+
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+)
+
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# 输出信息
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
+
+# 定义神经网络
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+# 实例化神经网络，并指定设备
+model = NeuralNetwork().to(device)
+print(model)
+
+# 测试，了解函数
+## 了解模型如何使用并产出预测值
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+print(logits)
+pred_probab = nn.Softmax(dim=1)(logits)
+print(pred_probab)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+print(f"First Linear weights: {model.linear_relu_stack[0].weight} \n")
+print(f"First Linear biases: {model.linear_relu_stack[0].bias} \n")
+
+## 了解Flatten函数的运作原理
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+## 了解Linear神经网络运作原理
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+## 了解ReLU函数
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
+
+## 了解nn.squential()
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20, 10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+
+## 了解nn.Softmax函数
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
+
+## 了解神经网络中参数基本遍历方式
+print("Model structure: ", model, "\n\n")
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+
+
+# torch.autograd 自动梯度下降
+import torch
+
+x = torch.ones(5)  # input tensor
+y = torch.zeros(3)  # expected output
+w = torch.randn(5, 3, requires_grad=True)
+b = torch.randn(3, requires_grad=True)
+print(w,b,'\n')
+z = torch.matmul(x, w)+b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
+
+print('Gradient function for z =',z.grad_fn)
+print('Gradient function for loss =', loss.grad_fn)
+
+loss.backward()
+print(w.grad)
+print(b.grad)
+
+# 禁用梯度追踪
+z = torch.matmul(x, w)+b
+print(z.requires_grad)
+
+with torch.no_grad():
+    z = torch.matmul(x, w)+b
+print(z.requires_grad)
+
+## 通过 detach() 也可以起到相同的作用
+z = torch.matmul(x, w)+b
+z_det = z.detach()
+t = z_det
+print(z_det.requires_grad)
+print(t.requires_grad)
+
+inp = torch.eye(5, requires_grad=True)
+out = (inp+1).pow(2)
+out.backward(torch.ones_like(inp), retain_graph=True)
+print("First call\n", inp.grad)
+out.backward(torch.ones_like(inp), retain_graph=True)
+print("\nSecond call\n", inp.grad)
+inp.grad.zero_()
+out.backward(torch.ones_like(inp), retain_graph=True)
+print("\nCall after zeroing gradients\n", inp.grad)
+
+
+# 优化参数循环建立
+## 设定超参
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+## 设定损失函数与优化算法
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+## 实现训练循环与测试循环
+### 训练循环
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        #### 计算预期值与损失值
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        #### 后向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+### 测试循环
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# 调用循环
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
